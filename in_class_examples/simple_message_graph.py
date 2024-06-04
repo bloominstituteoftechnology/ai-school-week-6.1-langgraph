f73eaa68ceb8554e54106f1cbe99c280d1dc814a
ashtilawat23 <ashtilawat23@gmail.com>
2024-06-04 10:24:54 -0400

    added example

---
+++ in_class_examples/simple_message_graph.py
@@ -0,0 +1,14 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langgraph.graph import END, MessageGraph
+
+model = ChatOpenAI(temperature=0)
+
+graph = MessageGraph()
+
+graph.add_node("oracle", model)
+graph.add_edge("oracle", END)
+
+graph.set_entry_point("oracle")
+
+runnable = graph.compile()
